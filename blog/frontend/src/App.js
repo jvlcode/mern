dad2dfc843a85476147ecd9a5846bd5e9a0dcfa1
JVLcode <81806680+jvlcode@users.noreply.github.com>
2024-07-29 19:37:02 +0530

    Header Footer and Detail component

--- blog/frontend/src/App.js
+++ blog/frontend/src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import PostDetail from './pages/PostDetail';
 import PostList from './pages/PostList';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <PostList/>
+        <Header/>
+          <Router>
+              <Routes>
+                  <Route path='/' element={<PostList/>} />
+                  <Route path='/posts/:id' element={<PostDetail/>} />
+              </Routes>
+          </Router>
+        <Footer/>
     </div>
   );
 }
--- blog/frontend/src/pages/PostList.js
+++ blog/frontend/src/pages/PostList.js
@@ -1,69 +1,32 @@
+import Post from "../components/Post";
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+
 export default function PostList() {
+    const [posts, setPosts] = useState([]);
+
+    const fetchPosts = async () => {
+        const response = await axios.get('http://localhost:8000/api/posts')
+        setPosts(response.data);
+    }
+
+
+    useEffect(() => {
+        fetchPosts();
+    }, [])
+
+
+
     return <>
-        <header>
-		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-			<div class="container">
-				<a class="navbar-brand" href="#">My Blog</a>
-				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-					aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<div class="collapse navbar-collapse" id="navbarNav">
-					<ul class="navbar-nav ms-auto">
-						<li class="nav-item">
-							<a class="nav-link active" aria-current="page" href="#">Home</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">Posts</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">About</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">Contact</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</nav>
-	</header>
 
 	<main>
 		<div class="container mt-4">
 			<div class="row">
 				<div class="col-lg-8">
 					<h1 class="mb-4">Latest Posts</h1>
-
-					<div class="card mb-4">
-						<div class="row">
-							<div class="col-sm-12 col-md-3">
-								<img class="img-fluid h-100 card-img-top" src="https://via.placeholder.com/800x400"
-									 alt="..." />
-							</div>
-							<div class="card-body col-md-8">
-								<h5 class="card-title">Post Title 1</h5>
-								<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-								<a href="#" class="btn btn-primary">Read More</a>
-							</div>
-						</div>
-
-					</div>
-
-					<div class="card mb-4">
-						<div class="row">
-							<div class="col-sm-12 col-md-3">
-								<img class="img-fluid h-100 card-img-top" src="https://via.placeholder.com/800x400"
-									 alt="..." />
-							</div>
-							<div class="card-body col-md-8">
-								<h5 class="card-title">Post Title 2</h5>
-								<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-								<a href="#" class="btn btn-primary">Read More</a>
-							</div>
-						</div>
-
-					</div>
-
+                    {
+                        posts.length > 0 ? posts.map((post) => <Post post={post} />) : <h4>No posts available</h4>
+                    }
 				</div>
 				<div class="col-lg-4">
 					<div class="card mb-4">
@@ -88,10 +51,5 @@ export default function PostList() {
         </div>
 	</main>
 
-	<footer class="bg-dark text-white text-center py-3 fixed-bottom">
-		<div class="container">
-			<p>&copy; 2024 My Blog. All rights reserved.</p>
-		</div>
-	</footer>
     </>
 }
